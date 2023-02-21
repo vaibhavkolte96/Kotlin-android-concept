63831db1355e75053793ab8560bfcd24c355328d
Vaibhav Kolte <vaibhav.kolte@exteral.forvia.com>
2023-02-21 11:02:27 +0530

    text, edittext, imageview

---
+++ app/src/main/java/com/vasu/kotlinconcept/EditImageViewActivity.kt
@@ -0,0 +1,11 @@
+package com.vasu.kotlinconcept
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditImageViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_image_view_anctivity)
+    }
+}
---
+++ app/src/main/java/com/vasu/kotlinconcept/TextUnderstandingActivity.kt
@@ -0,0 +1,11 @@
+package com.vasu.kotlinconcept
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextUnderstandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_unserdatanding)
+    }
+}
